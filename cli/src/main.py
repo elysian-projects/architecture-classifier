7bf5f1ffd24c3dbac7c8c031641150d0cead84bd
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-04-21 19:18:16 +0300

    chore: get the cli working

--- cli/src/main.py
+++ cli/src/main.py
@@ -1,27 +1,39 @@
 import numpy as np
-import tensorflow as tf
+import sys
 from utils import *
 
-CLASS_NAMES = ["Class 1", "Class 2", "Class 3", "Class 4", "Class 5"]
+args = sys.argv[1:]
 
-interpreter = tf.lite.Interpreter(model_path = resolve_model_path("flowers"))
+# The model must be passed explicitly as the first argument
+check_if_model_provided(args)
+
+MODEL_PATH = resolve_model_path(args[0])
+CLASS_NAMES = read_class_names()
+IMAGES = get_images_paths()
+
+# This function helps disable default tensorflow debug messages,
+# pass the `--show-tf-debug` flag to enable them
+disable_tensorflow_debugging_info_if_required(args)
+
+# Import the tensorflow module AFTER checking if the debug messages are enabled
+import tensorflow as tf
+
+interpreter = tf.lite.Interpreter(model_path = MODEL_PATH)
 interpreter.allocate_tensors()
 
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+input_size = interpreter.get_input_details()[0]["shape"]
 
-input_shape = input_details[0]['shape']
+IMAGE_WIDTH = input_size[1]
+IMAGE_HEIGHT = input_size[2]
 
-images = get_images_paths()
-image = resize_image(read_image(images[0]), 180, 180)
+for image in IMAGES:
+  image_as_array = resize_image(read_image(image), IMAGE_WIDTH, IMAGE_HEIGHT)
 
-input_data = np.array([image], dtype = np.float32)
+  input_data = np.array(np.expand_dims(image_as_array, axis = 0), dtype = np.float32)
 
-print(input_data)
+  interpreter.set_tensor(interpreter.get_input_details()[0]['index'], input_data)
+  interpreter.invoke()
 
-interpreter.set_tensor(input_details[0]['index'], input_data)
+  output_data = interpreter.get_tensor(interpreter.get_output_details()[0]['index'])
 
-interpreter.invoke()
-
-output_data = interpreter.get_tensor(output_details[0]['index'])
-print(output_data)
+  print_result(get_result(output_data[0], CLASS_NAMES), image)
--- cli/src/utils.py
+++ cli/src/utils.py
@@ -1,14 +1,24 @@
 from os import path as __path
 from os import listdir as __listdir
+from os import environ as __environ
 from PIL import Image
-import imageio as __iio
 import numpy as np
 
 __MODEL_BASE_DIR = "".join(__path.join(__path.dirname(__file__), "../models"))
 __IMAGE_BASE_DIR = "".join(__path.join(__path.dirname(__file__), "../images"))
+__CLASS_NAMES_PATH = "".join(__path.join(__path.dirname(__file__), "../classes.txt"))
 
 __MODEL_EXTENSION = "tflite"
 __IMAGE_EXTENSIONS = ["png", "jpg", "jpeg"]
+
+
+def dataset_exists(filename):
+  return __path.exists(__path.join(__MODEL_BASE_DIR, get_model_fullname(filename)))
+
+
+def read_class_names():
+  with open(__CLASS_NAMES_PATH, "r") as file:
+    return file.read().split("\n")
 
 
 def resolve_model_path(model_name: str) -> str:
@@ -16,23 +26,20 @@ def resolve_model_path(model_name: str) -> str:
   Resolves the model path by name. Looking for the model in the /cli/models folder
   NOTE: the name should be passed WITHOUT the extension
   '''
-  return __path.join(__MODEL_BASE_DIR, model_name + "." + __MODEL_EXTENSION)
+  return __path.join(__MODEL_BASE_DIR, get_model_fullname(model_name))
 
 
-def read_image(image_path):
-  return __iio.imread(image_path)
+def get_model_fullname(model_name: str) -> str:
+  return model_name + "." + __MODEL_EXTENSION
 
 
-def resize_image(image, width, height):
-  image = Image.fromarray(image)
-  image.thumbnail([width, height])
+def read_image(image_path) -> Image:
+  return Image.open(image_path).convert("RGB")
 
-  pix = np.array(image.getdata())
 
-  data = list(tuple(pixel) for pixel in pix)
-  image.putdata(data)
-
-  return np.array(image)
+def resize_image(image, width, height) -> None:
+  temp_image = image.resize([width, height])
+  return np.array(temp_image)
 
 
 def get_images_paths() -> list[str]:
@@ -51,3 +58,47 @@ def __is_image_file(filename: str) -> bool:
 
 def __is_available_image_extension(file: str) -> bool:
   return file.split(".")[-1] in __IMAGE_EXTENSIONS
+
+
+def get_result(model_output: list[float], classes: list[str]) -> str:
+  validate_output(model_output, classes)
+
+  max_value = -10000
+  max_index = 0
+
+  for i in range(len(model_output)):
+    if(model_output[i] > max_value):
+      max_value = model_output[i]
+      max_index = i
+
+  return classes[max_index]
+
+
+def print_result(result: str, filename: str) -> None:
+  normalized_filename = __path.normpath(filename)
+  separator = "/" if "/" in normalized_filename else "\\"
+  file_short_name = normalized_filename.split(separator)[-1]
+
+  print(f"{file_short_name}: {result}")
+
+
+def validate_output(model_output: list[float], classes: list[str]):
+  if(len(model_output) != len(classes)):
+    print(len(model_output), len(classes))
+    print("Model output length does not match the number of classes!")
+    exit(1)
+
+
+def check_if_model_provided(args):
+  if(len(args) == 0):
+    print("The model name must be provided!")
+    exit(1)
+
+  if(not dataset_exists(args[0])):
+    print(f"The model `{args[0]}.tflite` does not exist in the models directory!")
+    exit(1)
+
+
+def disable_tensorflow_debugging_info_if_required(args):
+  if(not "--show-tf-debug" in args):
+    __environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
