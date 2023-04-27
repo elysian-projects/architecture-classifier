8e12d362612d5185d86995ccd9500b0f175adb24
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-04-28 00:27:00 +0300

    fix: image uri -> bitmap converter

--- app/app/src/main/java/com/architecture/app/image/Utils.java
+++ app/app/src/main/java/com/architecture/app/image/Utils.java
@@ -2,7 +2,7 @@ package com.architecture.app.image;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.ImageDecoder;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 
 import androidx.annotation.Nullable;
@@ -11,7 +11,7 @@ public class Utils {
     @Nullable
     public static Bitmap getBitmapFromUri(Uri uri, Context context) {
         try {
-            return ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.getContentResolver(), uri));
+            return BitmapFactory.decodeStream(context.getContentResolver().openInputStream(uri));
         } catch(Exception exception) {
             return null;
         }
