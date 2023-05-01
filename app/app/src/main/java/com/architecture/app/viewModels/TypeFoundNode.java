7fdf05950c26bcdc48a556f3f1903248a422b74f
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-05-01 22:33:10 +0300

    chore: rename node types

---
+++ app/app/src/main/java/com/architecture/app/viewModels/TypeFoundNode.java
@@ -0,0 +1,11 @@
+package com.architecture.app.viewModels;
+
+public class TypeFoundNode {
+    public final String value;
+    public final int foundTimes;
+
+    public TypeFoundNode(String value, int foundTimes) {
+        this.value = value;
+        this.foundTimes = foundTimes;
+    }
+}
