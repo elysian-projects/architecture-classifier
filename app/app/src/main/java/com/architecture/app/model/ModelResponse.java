c7e9adaa2c6cc92c3d5ee915ddca134961a2bc25
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-05-03 13:20:02 +0300

    chore: add the `final` keyword to the constants

--- app/app/src/main/java/com/architecture/app/model/ModelResponse.java
+++ app/app/src/main/java/com/architecture/app/model/ModelResponse.java
@@ -1,8 +1,8 @@
 package com.architecture.app.model;
 
 public class ModelResponse {
-    public static String SUCCESSFUL_RESPONSE_SHORT = "С большой вероятностью, на фотографии изображён этот тип архитектуры";
-    public static String FAILED_RESPONSE_SHORT = "К сожалению, нам не удалось определить этот вид архитектуры";
+    public static final String SUCCESSFUL_RESPONSE_SHORT = "С большой вероятностью, на фотографии изображён этот тип архитектуры";
+    public static final String FAILED_RESPONSE_SHORT = "К сожалению, нам не удалось определить этот вид архитектуры";
 
     private final String _message;
     private final boolean _found;
