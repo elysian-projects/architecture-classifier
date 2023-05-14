e4d039970fedfe002ac36dc058ba14c3c8130972
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-05-15 01:57:23 +0300

    chore: get rid of the upload activity

--- app/app/src/main/java/com/architecture/app/components/Navbar.java
+++ app/app/src/main/java/com/architecture/app/components/Navbar.java
@@ -1,7 +1,5 @@
 package com.architecture.app.components;
 
-import android.content.Intent;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -12,7 +10,6 @@ import com.architecture.app.databinding.ActivityNavbarBinding;
 import com.architecture.app.screens.fragments.AchievementsFragment;
 import com.architecture.app.screens.fragments.HomeFragment;
 import com.architecture.app.screens.fragments.QuestionFragment;
-import com.architecture.app.screens.UploadActivity;
 import com.architecture.app.screens.fragments.UploadFragment;
 
 public class Navbar {
