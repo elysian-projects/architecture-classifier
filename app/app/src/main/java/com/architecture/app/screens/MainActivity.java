ac2b4be922be77e65ce7f64a6d03511de69b76dd
Matvey-Prus <mprusakov05@mail.ru>
2023-05-14 22:38:26 +0300

    fix:status bar color, action bar color, navigation bar color, deleted night theme

--- app/app/src/main/java/com/architecture/app/screens/MainActivity.java
+++ app/app/src/main/java/com/architecture/app/screens/MainActivity.java
@@ -1,10 +1,13 @@
 package com.architecture.app.screens;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.architecture.app.R;
 import com.architecture.app.components.Navbar;
@@ -17,5 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         new Navbar().attachToLayout(this);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+
     }
  }
