e2aecbb6aea70d29aaa5193bd17f11c5ef0e9526
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-05-15 21:18:38 +0300

    fix: data first load bug

--- app/app/src/main/java/com/architecture/app/constants/Assets.java
+++ app/app/src/main/java/com/architecture/app/constants/Assets.java
@@ -2,5 +2,5 @@ package com.architecture.app.constants;
 
 public class Assets {
     public static final String ARCHITECTURE_TYPES = "data/architecture_types.json";
-    public static final String TYPES_FOUND_DATA = "data/types_found.json";
+    public static final String TYPES_FOUND_DATA = "types_found.json";
 }
