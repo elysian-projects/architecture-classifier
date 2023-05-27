c678a364e24aceea825c4949a1f5dadd3d39dc7b
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-05-27 22:31:31 +0300

    chore: blackout not acquired achievements

--- app/app/src/main/java/com/architecture/app/utils/AchievementsGridAdapter.java
+++ app/app/src/main/java/com/architecture/app/utils/AchievementsGridAdapter.java
@@ -1,26 +1,26 @@
 package com.architecture.app.utils;
 
 import android.content.Context;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.architecture.app.R;
 import com.architecture.app.components.dialog.DialogVariant;
 import com.architecture.app.components.dialog.DialogWindow;
-import com.architecture.app.databinding.AchievementNotificationBinding;
 import com.architecture.app.databinding.RowAchievementBinding;
 import com.architecture.app.viewModels.Achievement;
-import com.google.android.material.snackbar.Snackbar;
+import com.architecture.app.viewModels.AcquiredAchievementNode;
 
 import java.io.IOException;
 
 public class AchievementsGridAdapter extends RecyclerView.Adapter<AchievementsGridAdapter.AchievementsViewHolder> {
+    private AcquiredAchievementNode[] _acquiredAchievements;
     private final Achievement[] _achievements;
 
     public AchievementsGridAdapter(Achievement[] achievements) {
@@ -47,12 +47,21 @@ public class AchievementsGridAdapter extends RecyclerView.Adapter<AchievementsGr
 
     @Override
     public void onBindViewHolder(@NonNull AchievementsViewHolder holder, int position) {
+        if(_acquiredAchievements == null) {
+            try {
+                loadAcquiredAchievements(holder.itemView.getContext());
+                Log.w("AchievementsGridAdapter", "CALL");
+            } catch (IOException exception) {
+                Log.e("AchievementsGridAdapter", "Could not load acquired achievements", exception);
+            }
+        }
+
         Achievement achievement = _achievements[position];
         Context context = holder.itemView.getContext();
 
         DialogWindow dialog = new DialogWindow(context);
 
-        loadImageToRow(holder.binding.achievementImagePreview, achievement.preview, context);
+        loadImageToRow(holder.binding.achievementImagePreview, achievement.preview, isAcquired(achievement), context);
 
         holder.binding.achievementImagePreview.setOnClickListener(view -> {
             try {
@@ -72,11 +81,31 @@ public class AchievementsGridAdapter extends RecyclerView.Adapter<AchievementsGr
         return _achievements.length;
     }
 
-    private void loadImageToRow(ImageView imageView, String preview, Context context) {
+    private void loadImageToRow(ImageView imageView, String preview, boolean acquired, Context context) {
         try {
             imageView.setImageBitmap(AssetsParser.readPreviewImage(context, preview));
+
+            if(!acquired) {
+                ColorMatrix matrix = new ColorMatrix();
+                matrix.setSaturation(0);
+                imageView.setColorFilter(new ColorMatrixColorFilter(matrix));
+            }
         } catch(Exception exception) {
             Log.i("AchievementsFragment", "Could not load a preview image", exception);
         }
+    }
+
+    private void loadAcquiredAchievements(Context context) throws IOException {
+        _acquiredAchievements = AssetsAchievements.parseAcquiredAchievementsData(context);
+    }
+
+    private boolean isAcquired(Achievement achievement) {
+        for(AcquiredAchievementNode node : _acquiredAchievements) {
+            if(node.condition.equalsIgnoreCase(achievement.condition) && node.acquired) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
