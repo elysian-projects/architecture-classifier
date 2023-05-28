c30bdde6154ce3db5f14715edb49a3272dfa4868
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-05-29 00:09:02 +0300

    chore: make navbar set titles

--- app/app/src/main/java/com/architecture/app/components/Navbar.java
+++ app/app/src/main/java/com/architecture/app/components/Navbar.java
@@ -1,5 +1,7 @@
 package com.architecture.app.components;
 
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -21,38 +23,50 @@ public class Navbar {
     }
 
     private void initializeButtons(ActivityNavbarBinding binding, AppCompatActivity context) {
-        replaceFragment(new HomeFragment(), context);
+        replaceFragment(new HomeFragment(), context.getString(R.string.app_name), context);
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             Fragment fragmentToLoad;
+            String title;
 
             switch(item.getItemId()) {
                 case R.id.home:
                     fragmentToLoad = new HomeFragment();
+                    title = context.getString(R.string.app_name);
                     break;
                 case R.id.upload:
                     fragmentToLoad = new UploadFragment();
+                    title = "Загрузка изображений";
                     break;
                 case R.id.check:
                     fragmentToLoad = new AchievementsFragment();
+                    title = "Достижения";
                     break;
                 case R.id.question:
                     fragmentToLoad = new QuestionFragment();
+                    title = "QUIZ";
                     break;
                 default:
                     return false;
             }
 
-            replaceFragment(fragmentToLoad, context);
+            replaceFragment(fragmentToLoad, title, context);
 
             return true;
         });
     }
 
-    private void replaceFragment(Fragment fragment, AppCompatActivity context) {
+    private void replaceFragment(Fragment fragment, String title, AppCompatActivity context) {
         FragmentManager fragmentManager = context.getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+        fragmentTransaction.runOnCommit(() -> {
+            try {
+                context.getSupportActionBar().setTitle(title);
+            } catch(NullPointerException exception) {
+                Log.w("Navbar", exception.getMessage(), exception);
+            }
+        });
     }
 }
